60a48b9fe48449c348fd3f4f47b251ce571380cc
Anastasia <av@anastasia.io>
2018-02-13 11:32:17 -0800

    pointer events not working yet

--- VR_OS/Assets/_Scripts/Menus/UICuffQuickAppMenu.cs
+++ VR_OS/Assets/_Scripts/Menus/UICuffQuickAppMenu.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class UICuffQuickAppMenu : MonoBehaviour {
 
+    private static bool menuState = false; //menu active or inactive .. can use visibility 
+    //reference to the child meshes for layout
+
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        //add layout logic 
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    //toggle menu visibility
+    public void toggleMenu()
+    {
+        menuState = !menuState;
+        this.gameObject.SetActive(menuState);
+        print(menuState);
+    }
+
+
 }
